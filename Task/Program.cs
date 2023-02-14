4cce2673874f8818a94fba8a2a8754fe8ae68719
Nikita <awardow@yandex.ru>
2023-02-14 18:56:11 +0300

    The basis of the program

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,3 +7,10 @@
 ["1234", "1567", "-2", "computer science"] -> ["-2"]
 ["Russia", "Denmark", "Kazan"] -> []
 */
+
+string[] array = CreatingArray();
+string[] newArray = TaskSolution(array);
+
+Console.Write($"[\"{String.Join("\", \"", array)}\"] -> [\"{String.Join("\", \"", DeleteNull(newArray))}\"]");
+
+//=====================Methods=============================
